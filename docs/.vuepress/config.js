c71c02aba437aa476e9688a29703c440c76fce55
swp <3297943361@qq.com>
2024-07-03 18:32:01 +0800

    commit2

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { webpackBundler } from '@vuepress/bundler-webpack'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+
+  title: 'VuePres',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+
+  }),
+
+
+  bundler: webpackBundler(),
+})
